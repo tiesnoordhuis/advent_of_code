46ada8ed49356d7355999418911979e7451da498
ties <ties@bit-students.com>
2018-12-05 19:59:36 +0100

    longest sleep determent

--- advent_of_code_2018/python/day4.py
+++ advent_of_code_2018/python/day4.py
@@ -43,7 +43,6 @@ for parsed_event in parsed_events:
     if parsed_event["event_data"][0] == "w":
         grouped_event["awake"].append(parsed_event["date"])
     prev_event = parsed_event
-    print(grouped_event)
     first = False
 
 parsed_grouped_event = {}
@@ -55,16 +54,61 @@ for grouped_event in grouped_events:
         parsed_grouped_event["day"] = np.datetime64(grouped_event["start"], "D")
     else:
         parsed_grouped_event["day"] = np.datetime64(grouped_event["start"] + np.timedelta64(1, "h"), "D")
-
     parsed_grouped_event["delta_awake"] = [0]
     for wake_date in grouped_event["awake"]:
         delta = parsed_grouped_event["day"] - wake_date
+        delta = delta / np.timedelta64(1, 'm')
+        delta = int(0 - delta)
         parsed_grouped_event["delta_awake"].append(delta)
-
     parsed_grouped_event["delta_asleep"] = []
     for wake_date in grouped_event["asleep"]:
         delta = parsed_grouped_event["day"] - wake_date
+        delta = delta / np.timedelta64(1, 'm')
+        delta = int(0 - delta)
         parsed_grouped_event["delta_asleep"].append(delta)
+    parsed_grouped_events.append(parsed_grouped_event.copy())
 
-    print(parsed_grouped_event)
-    parsed_grouped_events.append(parsed_grouped_event)
+complete_event = {}
+complete_events = []
+
+for parsed_grouped_event in parsed_grouped_events:
+    complete_event["ID"] = parsed_grouped_event["ID"]
+    complete_event["day"] = parsed_grouped_event["day"]
+    complete_event["schedule"] = []
+    for minute in range(60):
+        if guard_awake:
+            if minute in parsed_grouped_event["delta_asleep"]:
+                guard_awake = False
+        if guard_awake == False:
+            if minute in parsed_grouped_event["delta_awake"]:
+                guard_awake = True
+        complete_event["schedule"].append((minute, guard_awake))
+    complete_event["awake_time"] = 0
+    for active_minute in complete_event["schedule"]:
+        if active_minute[1]:
+            complete_event["awake_time"] += 1
+    complete_event["sleep_time"] = 60 - complete_event["awake_time"]
+    complete_events.append(complete_event.copy())
+    print(complete_event)
+
+list_IDs = []
+sleep_times = {}
+
+for event in complete_events:
+    if event["ID"] in list_IDs:
+        sleep_times[event["ID"]] += event["sleep_time"]
+    else:
+        sleep_times[event["ID"]] = event["sleep_time"]
+        list_IDs.append(event["ID"])
+
+print(sleep_times)
+
+longest_sleep_time = 0
+longest_sleep_ID = ""
+
+for item_key in sleep_times:
+    if sleep_times[item_key] > longest_sleep_time:
+        longest_sleep_time = sleep_times[item_key]
+        longest_sleep_ID = item_key
+
+print(longest_sleep_ID)
