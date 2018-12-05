504c4d571ebd5432850034cffc0fa1e8292fc9c9
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-12-05 16:51:16 +0100

    continuation of day 4, added more parsing

--- advent_of_code_2018/python/day4.py
+++ advent_of_code_2018/python/day4.py
@@ -22,13 +22,40 @@ parsed_events = [i for i in range(len(events))]
 
 for index, sorting_event in enumerate(sorting_events):
     parsed_events[index] = parsed_events_unsorted[sorting_event[1]]
-    print(parsed_events[index])
-
-print(parsed_events)
 
 grouped_events = []
 grouped_event = {}
+prev_event = {}
+first = True
 
 for parsed_event in parsed_events:
     if parsed_event["event_data"][0] == "G":
-        grouped_event["start"]
+        if first == False:
+            grouped_event["end"] = prev_event["date"]
+            grouped_events.append(grouped_event)
+        grouped_event = {}
+        grouped_event["start"] = parsed_event["date"]
+        grouped_event["ID"] = parsed_event["event_data"].split()[1][1:]
+        grouped_event["asleep"] = []
+        grouped_event["awake"] = []
+    if parsed_event["event_data"][0] == "f":
+        grouped_event["asleep"].append(parsed_event["date"])
+    if parsed_event["event_data"][0] == "w":
+        grouped_event["awake"].append(parsed_event["date"])
+    prev_event = parsed_event
+    first = False
+
+parsed_grouped_event = {}
+parsed_grouped_events = []
+guard_awake = True
+for grouped_event in grouped_events:
+    parsed_grouped_event["ID"] = grouped_event["ID"]
+    if np.datetime64(grouped_event["start"], "D") == np.datetime64(grouped_event["start"] + np.timedelta64(1, "h"), "D"):
+        parsed_grouped_event["day"] = np.datetime64(grouped_event["start"], "D")
+    else:
+        parsed_grouped_event["day"] = np.datetime64(grouped_event["start"] + np.timedelta64(1, "h"), "D")
+    parsed_grouped_event["schedule"] = []
+    for minute in range(60):
+        parsed_grouped_event["schedule"].append((minute, guard_awake))
+    print(parsed_grouped_event)
+    parsed_grouped_events.append(parsed_grouped_event)
