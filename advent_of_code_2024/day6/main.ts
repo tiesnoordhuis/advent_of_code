5dd6224c7995cca548aed8951f67e8615781707e
Ties Noordhuis <tiesnoordhuis@gmail.com>
2024-12-23 12:26:14 +0100

    day 6 part 2

--- advent_of_code_2024/day6/main.ts
+++ advent_of_code_2024/day6/main.ts
@@ -23,27 +23,25 @@ class Guard {
     position: Vector
     mapSize: Vector
     visitedLocations: Set<string>
+    visitedLocationsWithDirections: Set<string>
+    hasLooped: boolean = false
 
     constructor(startLocation: Vector, startDirection: UnitVector, mapSize: Vector) {
         this.position = startLocation
         this.facing = startDirection
         this.mapSize = mapSize
         this.visitedLocations = new Set([`x${this.position.x}, y${this.position.y}`])
+        this.visitedLocationsWithDirections = new Set([`x${this.position.x}, y${this.position.y}, dx${this.facing.x}, dy${this.facing.y}`])
     }
 
     move() {
         this.position.x += this.facing.x
         this.position.y += this.facing.y
-        this.visitedLocations.add(`x${this.position.x}, y${this.position.y}`)
+        this.addToVisited()
     }
 
     turnIfObstructed(map: boolean[][]) {
-        let counter = 0 
         while (map[this.position.y + this.facing.y][this.position.x + this.facing.x]) {
-            counter++
-            if (counter > 1) {
-                console.log(guard);   
-            }
             switch (this.facing) {
                 case Directions.up:
                     this.facing = Directions.right
@@ -63,6 +61,16 @@ class Guard {
         }
     }
 
+    addToVisited() {
+        this.visitedLocations.add(`x${this.position.x}, y${this.position.y}`)
+        const locationWithDirection = `x${this.position.x}, y${this.position.y}, dx${this.facing.x}, dy${this.facing.y}`
+        if (this.visitedLocationsWithDirections.has(locationWithDirection)) {
+            this.hasLooped = true
+            return
+        }
+        this.visitedLocationsWithDirections.add(locationWithDirection)
+    }
+
     get reachedBorder(): boolean {
         return (
             this.position.x + this.facing.x > this.mapSize.x
@@ -79,13 +87,37 @@ const startX: number = data[startY].indexOf('^')
 
 const guard = new Guard({x: startX, y: startY}, Directions.up, {x: map[0].length - 1, y: map.length - 1})
 
-for (let index = 0; index < 1000000; index++) {
+while(!guard.reachedBorder) {
     guard.turnIfObstructed(map)
     guard.move()
-    if (guard.reachedBorder) {
-        break
-    }
 }
-// console.log(guard.visitedLocations)
 console.log(guard.visitedLocations.size);
+console.log(guard.visitedLocationsWithDirections.size);
+
+const mapsWithObstruction: boolean[][][] = []
+
+guard.visitedLocations.forEach((visitedLocation) => {
+    const [xString, yString] = visitedLocation.split(', ')
+    const x: number = Number(xString.substring(1))
+    const y: number = Number(yString.substring(1))
+    const newMap = map.map((row) => [...row])
+    newMap[y][x] = true
+    mapsWithObstruction.push(newMap)
+})
+
+let loopedCounter = 0;
+
+mapsWithObstruction.forEach((mapOption) => {
+    const guard = new Guard({x: startX, y: startY}, Directions.up, {x: map[0].length - 1, y: map.length - 1})
+    while (!guard.reachedBorder) {
+        guard.turnIfObstructed(mapOption)
+        guard.move()
+        if (guard.hasLooped) {
+            loopedCounter++
+            break
+        }
+    }
+})
+console.log(loopedCounter);
+
 
